07735dea8f92cebccbc8dc0edcac17e96dd7eb1c
Aurelion_Sol <essamwisam@outlook.com>
2020-05-09 22:44:00 +0200

    Added support for spherical and cylindrical coordinates.

--- Thermodynamics/Thermodynamics/Thermodynamics.py
+++ Thermodynamics/Thermodynamics/Thermodynamics.py
@@ -1,12 +1,13 @@
 import PySimpleGUI as GUI   #For some decent UI (check the documentation/cook book because it will help : https://pysimplegui.readthedocs.io/en/latest/)
 import numpy as np             
 import matplotlib.pyplot as plt  #This and the previous library are for graphing
-
+from numpy import log
 
 #=======================================================================================================================================================#
 
 #The layout of the GUI, Note that the first pair of square brackets is for the layout list and each succeeding pair represents a row.
 #We have 2 rows, the first for the "Choose Coordinate System" text, the second is for the 3 buttons
+GUI.theme('Material1')
 layout = [[GUI.T('Choose Coordinate System')],[GUI.B('Cartesian'),GUI.B('Cylinderical'), GUI.B('Spherical')]]
 
 #Creates a window with the specified name and layout and stores it in the variable "window"
@@ -110,11 +111,187 @@ while True:
 
 
     elif event == "Cylinderical": #Cylinderical Code
-        GUI.popup("You forgot to put cylinderical code in...")
+         while True: #The windows has to be running until the user exits (breaks)
+            #Input Window
+            GUI.theme('Material1')  # there
+            # Creating Window Text, Text Boxes and Buttons
+            Layout = [
+              [GUI.Text('                       Medium Info in SI Units  ')],
+              [GUI.Text('Temperature at the left border (Tₒ) :')], [GUI.InputText()],
+              [GUI.Text('Temperature at the right border ( Tₛ) :')], [GUI.InputText()],
+              [GUI.Text('Constant Kₒ :')], [GUI.InputText()],
+              [GUI.Text('Constant β  :')],[ GUI.InputText()],
+              [GUI.Text('Inner Radius (Rₒ) :')],[ GUI.InputText()],
+              [GUI.Text('Outer Radius (Rₛ) :')],[ GUI.InputText()],
+              [GUI.Text('Calculation Point (r) :')],[ GUI.InputText()],
+              [GUI.Text('The Cross-sectional Area (A) : ')],[ GUI.InputText()],
+              [GUI.Button('Scan'), GUI.Button('Quit')]
+             ]
+        
+
+            # Creating the initialized window
+            window = GUI.Window('Heat Radar', Layout)
+
+            # Interacting to the users clicks
+            while True:
+                Click, V = window.read() #This returns actions in 'Click' and entereds Variables in V[i]
+                if Click in (None, 'Quit'):   # if the user clicks Quit
+                    window.close()
+                    main_window.close() #Added by Tarek
+              
+                elif Click in ( 'Scan'):        #if the user clicks Scan
+                    if not (V[0] and V[1] and V[2] and V[3] and V[4] and V[5] and V[6] and V[7]): 
+                        #V[0] 0 corresponds to Tₒ V [1] corresponds to  Tₛ and so on, here we're just checking if any of them is not given (null)
+                        GUI.theme('Material1') #in case any of them isn't given we create a new window
+                        Layout= [  [GUI.Text('Some Variables are missing, please fill the list')],
+                        [GUI.Button('Back')] ]
+                        newwindow=GUI.Window('Missing Info', Layout)
+                        while True:
+                            newClick, newV = newwindow.read() 
+                            if newClick in ( None, 'Back'): #in the new window if the user clicks back it exits the new window (goes to newwindow.close())
+                                break
+                        newwindow.close()
+                    else: #otherwise if all Variables are given, a different window with the solution has to appear
+                        GUI.theme('Material1')
+            
+                        T0,TF,K0,B,R0,RF,r,A=float(V[0]),float(V[1]),float(V[2]),float(V[3]),float(V[4]),float(V[5]),float(V[6]),float(V[7])
+                        C1=K0*((TF+0.5*B*(TF**2))-(T0+0.5*B*(T0**2)))/log(RF/R0);
+                        C0=K0*(TF+0.5*B*(TF**2))-C1*log(RF);
+                        TSolution= (-K0+(K0**2+2*K0*B*(C1*log(r)+C0))**0.5)/(K0*B) #T Equations
+                        QSolution= A*(-C1)/r #Q Equation
+                        if(RF<R0):
+                            GUI.popup("The outer radius should be larger than the inner radius")
+                            break
+                        elif(r>=R0 and r<=RF): #We can only find T and Q within the boundary
+                            Layout= [  [GUI.Text('Laplace Equation Solves To : ')],
+                                 [GUI.Text( 'T = ('+str(-K0)+'+ ('+str(round(K0**2+C0*B*K0*2,3))+'+'+str(round(B*K0*2*C1,3))+'ln(r))'+'^-0.5'+')'+'/'+str(K0*B)+' where r ∈ ['+str(R0)+' , '+str(RF)+']')], 
+                                  [GUI.Text('Which corresponds to a temperature of '+str(round(TSolution,3))+' Kelvins and a heat flow rate of '+str(round(QSolution,3))+' Watts at r='+str(r))],#as well as the solution at the specified point
+                                [GUI.Button('Graph')],
+                                  [GUI.Button('Try different inputs')],
+                                 [GUI.Button('Quit')]]
+                        else :
+                            Layout= [  [GUI.Text('Laplace Equation Solves To : ')],
+                                 [GUI.Text( 'T = ('+str(-K0)+'+ ('+str(round(K0**2+C0*B*K0*2,3))+'+'+str(round(B*K0*2*C1,3))+'ln(r))'+'^-0.5'+')'+'/'+str(K0*B)+' where r ∈ ['+str(R0)+' , '+str(RF)+']')], 
+                                  [GUI.Text('Which corresponds to an unknown temperature and an unknown heat flow rate at r='+str(r))],#as well as the solution at the specified point
+                                [GUI.Button('Graph')],
+                                  [GUI.Button('Try different inputs')],
+                                 [GUI.Button('Quit')]]
+
+
+
+                        newwindow_1=GUI.Window('Solution', Layout) #creating the initialized window
+                
+                        # you can take the code in another python window and see how graphing works
+                        while True:
+                            newClick_1, newV_1 = newwindow_1.read()
+                            if newClick_1 in ( 'Graph'): 
+                                def graph (formula,range):
+                                    r=np.array(range)
+                                    T=formula(r)
+                                    plt.plot(r,T)
+                                    plt.show(block=False)
+                                def my_formula(r):
+                                    return (-K0+(K0**2+2*K0*B*(C1*log(r)+C0))**0.5)/(K0*B)
+                                graph(my_formula, range(int(V[4]), int(V[5])))
+                       
+                            if newClick_1 in ( None, 'Try different inputs'): #the user chooses to try a different point, so we exit our new windows
+                                break
+                            if newClick_1 in (None, 'Quit' ): #here we exit both windows
+                                window.close()
+                                break
+                        newwindow_1.close()
 
 
 
 
     elif event == "Spherical":  #Spherical Code
-        GUI.popup("You forgot to put spherical code in...")
+        while True: #The windows has to be running until the user exits (breaks)
+            #Input Window
+            GUI.theme('Material1')  # there
+            # Creating Window Text, Text Boxes and Buttons
+            Layout = [
+              [GUI.Text('                       Medium Info in SI Units  ')],
+              [GUI.Text('Temperature at the left border (Tₒ) :')], [GUI.InputText()],
+              [GUI.Text('Temperature at the right border ( Tₛ) :')], [GUI.InputText()],
+              [GUI.Text('Constant Kₒ :')], [GUI.InputText()],
+              [GUI.Text('Constant β  :')],[ GUI.InputText()],
+              [GUI.Text('Inner Radius (Rₒ) :')],[ GUI.InputText()],
+              [GUI.Text('Outer Radius (Rₛ) :')],[ GUI.InputText()],
+              [GUI.Text('Calculation Point (r) :')],[ GUI.InputText()],
+              [GUI.Text('The Cross-sectional Area (A) : ')],[ GUI.InputText()],
+              [GUI.Button('Scan'), GUI.Button('Quit')]
+             ]
+        
+
+            # Creating the initialized window
+            window = GUI.Window('Heat Radar', Layout)
+
+            # Interacting to the users clicks
+            while True:
+                Click, V = window.read() #This returns actions in 'Click' and entereds Variables in V[i]
+                if Click in (None, 'Quit'):   # if the user clicks Quit
+                    window.close()
+                    main_window.close() #Added by Tarek
+              
+                elif Click in ( 'Scan'):        #if the user clicks Scan
+                    if not (V[0] and V[1] and V[2] and V[3] and V[4] and V[5] and V[6] and V[7]): 
+                        #V[0] 0 corresponds to Tₒ V [1] corresponds to  Tₛ and so on, here we're just checking if any of them is not given (null)
+                        GUI.theme('Material1') #in case any of them isn't given we create a new window
+                        Layout= [  [GUI.Text('Some Variables are missing, please fill the list')],
+                        [GUI.Button('Back')] ]
+                        newwindow=GUI.Window('Missing Info', Layout)
+                        while True:
+                            newClick, newV = newwindow.read() 
+                            if newClick in ( None, 'Back'): #in the new window if the user clicks back it exits the new window (goes to newwindow.close())
+                                break
+                        newwindow.close()
+                    else: #otherwise if all Variables are given, a different window with the solution has to appear
+                        GUI.theme('Material1')
+            
+                        T0,TF,K0,B,R0,RF,r,A=float(V[0]),float(V[1]),float(V[2]),float(V[3]),float(V[4]),float(V[5]),float(V[6]),float(V[7])
+                        C1=K0*((TF+0.5*B*(TF**2))-(T0+0.5*B*(T0**2)))*(RF*R0/(RF-R0));
+                        C0=K0*(TF+0.5*B*(TF**2))+C1/RF;
+                        TSolution= (-K0+(K0**2+2*K0*B*(-C1/r+C0))**0.5)/(K0*B) #T Equations
+                        QSolution= A*(-C1)/r**2 #Q Equation
+                        if(RF<R0):
+                            GUI.popup("The outer radius should be larger than the inner radius")
+                            break
+                        elif(r>=R0 and r<=RF): #We can only find T and Q within the boundary
+                            Layout= [  [GUI.Text('Laplace Equation Solves To : ')],
+                                 [GUI.Text( 'T = ('+str(-K0)+'+ ('+str(round(K0**2+C0*B*K0*2,3))+'+'+str(round(B*K0*2*C1,3))+'-1/r)'+'^-0.5'+')'+'/'+str(K0*B)+' where r ∈ ['+str(R0)+' , '+str(RF)+']')], 
+                                  [GUI.Text('Which corresponds to a temperature of '+str(round(TSolution,3))+' Kelvins and a heat flow rate of '+str(round(QSolution,3))+' Watts at r='+str(r))],#as well as the solution at the specified point
+                                [GUI.Button('Graph')],
+                                  [GUI.Button('Try different inputs')],
+                                 [GUI.Button('Quit')]]
+                        else :
+                            Layout= [  [GUI.Text('Laplace Equation Solves To : ')],
+                                 [GUI.Text( 'T = ('+str(-K0)+'+ ('+str(round(K0**2+C0*B*K0*2,3))+'+'+str(round(B*K0*2*C1,3))+'-1/r)'+'^-0.5'+')'+'/'+str(K0*B)+' where r ∈ ['+str(R0)+' , '+str(RF)+']')], 
+                                  [GUI.Text('Which corresponds to an unknown temperature and an unknown heat flow rate at r='+str(r))],#as well as the solution at the specified point
+                                [GUI.Button('Graph')],
+                                  [GUI.Button('Try different inputs')],
+                                 [GUI.Button('Quit')]]
+
+
+
+                        newwindow_1=GUI.Window('Solution', Layout) #creating the initialized window
+                
+                        # you can take the code in another python window and see how graphing works
+                        while True:
+                            newClick_1, newV_1 = newwindow_1.read()
+                            if newClick_1 in ( 'Graph'): 
+                                def graph (formula,range):
+                                    r=np.array(range)
+                                    T=formula(r)
+                                    plt.plot(r,T)
+                                    plt.show(block=False)
+                                def my_formula(r):
+                                    return (-K0+(K0**2+2*K0*B*(-C1/r+C0))**0.5)/(K0*B)
+                                graph(my_formula, range(int(V[4]), int(V[5])))
+                       
+                            if newClick_1 in ( None, 'Try different inputs'): #the user chooses to try a different point, so we exit our new windows
+                                break
+                            if newClick_1 in (None, 'Quit' ): #here we exit both windows
+                                window.close()
+                                break
+                        newwindow_1.close()
          
